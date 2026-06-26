ecb4d4ad52250ff6c7bc3381fa35c2a6882b127b
Anis Alkhatib <alkhatib.anis@gmail.com>
2026-06-26 20:43:45 +0300

    complete exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,23 +1,43 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { id: 1, name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
-
-  const handleChange = (e) => {
-    setNewName(e.target.value);
-  };
+  const [newNumber, setNewNumber] = useState("");
 
   const addName = (e) => {
     e.preventDefault();
+
+    const currentMaxId =
+      persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+
+    const getMatchingName = (num) => {
+      return persons.find((n) => n.number === num)?.name || "Unknown";
+    };
 
     if (persons.some((n) => n.name.toLowerCase() === newName.toLowerCase())) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
 
-    setPersons(persons.concat({ name: newName }));
+    if (persons.some((n) => n.number === newNumber)) {
+      alert(
+        `${newNumber} is already added to phonebook for contact ${getMatchingName(newNumber)}`,
+      );
+      return;
+    }
+
+    const nameObject = {
+      id: currentMaxId + 1,
+      name: newName,
+      number: String(newNumber),
+    };
+
+    setPersons(persons.concat(nameObject));
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -25,18 +45,33 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name:{" "}
+          <input
+            type="text"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+          <div>
+            number:{" "}
+            <input
+              type="text"
+              value={newNumber}
+              onChange={(e) => setNewNumber(e.target.value)}
+            />
+          </div>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>
+      <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.id}>
+            {person.name} {person.number}
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };

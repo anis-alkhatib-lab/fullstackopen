56d208dcf20c75d7bb7bc5649c141838a75b1906
Anis Alkhatib <alkhatib.anis@gmail.com>
2026-07-24 01:49:15 -0700

    complete exercise 4.4

--- part4/blog-list/tests/list_helpers.test.js
+++ part4/blog-list/tests/list_helpers.test.js
@@ -1,8 +1,21 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const { listWithOneBlog, listWithManyBlogs } = require('../tests/tests_data')
 
 test('dummy return one', () => {
   const result = listHelper.dummy([])
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  test('when list has only one blog, equals the likes of that blog', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('when list has many blogs, equels the sum of likes for all blogs', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    assert.strictEqual(result, 36)
+  })
 })

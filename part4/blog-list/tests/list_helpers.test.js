a36c99932c5ddcae0cfb68acd623951bf56da735
Anis Alkhatib <alkhatib.anis@gmail.com>
2026-07-24 02:00:14 -0700

    complete exercise 4.5

--- part4/blog-list/tests/list_helpers.test.js
+++ part4/blog-list/tests/list_helpers.test.js
@@ -9,6 +9,11 @@ test('dummy return one', () => {
 })
 
 describe('total likes', () => {
+  test('when list is empty, return 0', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
   test('when list has only one blog, equals the likes of that blog', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     assert.strictEqual(result, 5)
@@ -17,5 +22,22 @@ describe('total likes', () => {
   test('when list has many blogs, equels the sum of likes for all blogs', () => {
     const result = listHelper.totalLikes(listWithManyBlogs)
     assert.strictEqual(result, 36)
+  })
+})
+
+describe('favorite blog', () => {
+  test('when list is empty return an empty object', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.deepStrictEqual(result, {})
+  })
+
+  test('when list has one blog return that blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    assert.deepStrictEqual(result, listWithOneBlog[0])
+  })
+
+  test('when list has many blogs, return the one with most likes', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    assert.deepStrictEqual(result, listWithManyBlogs[2])
   })
 })
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, item) => sum + item.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((fav, current) => (fav.likes > current.likes ? fav : current), {})
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
